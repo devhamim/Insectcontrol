c5141b7cf189fc5b86c753e23ec0ffb1ce7db3d1
devhamim <hamim735@gmail.com>
2024-06-25 17:22:40 +0600

    sitebar

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -92,41 +92,12 @@
                     </a>
                 </li>
                 <!-- Couriar API list -->
-                <li>
+                {{-- <li>
                     <a class="sidenav-item-link" href="{{ route('commingsoon') }}">
                         <i class="mdi mdi-car"></i>
                         <span class="nav-text">Couriar API</span>
                     </a>
-                </li>
-                <hr>
-                <!-- Expence list -->
-                <li>
-                    <a class="sidenav-item-link" href="{{ route('commingsoon') }}">
-                        <i class="mdi mdi-account-group"></i>
-                        <span class="nav-text">Expence</span>
-                    </a>
-                </li>
-                <!-- Purces list -->
-                <li>
-                    <a class="sidenav-item-link" href="{{ route('commingsoon') }}">
-                        <i class="mdi mdi-account-group"></i>
-                        <span class="nav-text">Purces</span>
-                    </a>
-                </li>
-                <!-- Markating Expence list -->
-                <li>
-                    <a class="sidenav-item-link" href="{{ route('commingsoon') }}">
-                        <i class="mdi mdi-account-group"></i>
-                        <span class="nav-text">Markating Expence</span>
-                    </a>
-                </li>
-                <!-- Officel Expence list -->
-                <li>
-                    <a class="sidenav-item-link" href="{{ route('commingsoon') }}">
-                        <i class="mdi mdi-account-group"></i>
-                        <span class="nav-text">Officel Expence</span>
-                    </a>
-                </li>
+                </li> --}}
                 <hr>
                 <!-- Markating setting list -->
                 <li>
@@ -136,12 +107,12 @@
                     </a>
                 </li>
                 <!-- Landing Page list -->
-                <li>
+                {{-- <li>
                     <a class="sidenav-item-link" href="{{ route('commingsoon') }}">
                         <i class="mdi mdi-book-open"></i>
                         <span class="nav-text">Landing Page</span>
                     </a>
-                </li>
+                </li> --}}
                 <!-- Visitor Count list -->
                 <li>
                     <a class="sidenav-item-link" href="{{ route('visitors') }}">
